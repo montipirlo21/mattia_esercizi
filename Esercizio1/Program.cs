a562748cb781b547fe3adae253393e6e095b27b1
Davide Monticelli <davide.monticelli@hotmail.it>
2025-01-27 21:30:00 +0100

    completato esercizio 1

--- Esercizio1/Program.cs
+++ Esercizio1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// Crea un programma che richieda all'utente di inserire il proprio nome e lo stampi.
-Console.WriteLine("Esercizio 1");
+public partial class Program
+{
+    public static void Main()
+    {
+        // Richiedo il nome
+        Console.WriteLine("Inserisci il tuo nome:");
+
+        // Attendo che lo legga
+        string nome = Console.ReadLine();
+
+        // Lo stampo a video     
+        Console.WriteLine("Il tuo nome è " + nome);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
