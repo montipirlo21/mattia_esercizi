154c6e9ba11a21a3e196e33dfe09b1c362e8e586
Davide Monticelli <davide.monticelli@hotmail.it>
2025-01-27 21:52:21 +0100

    fatto esercizio sui clici

--- Esercizio2/Program.cs
+++ Esercizio2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Esercizio 2");
+﻿// Fai un programma che faccia finta di suonare tre volte ad un campanello ( writeline "DONG)
+//e attendi facendo una readline
+Console.WriteLine("Arrivo a casa del mio amico");
+
+// Fallo 3 volte
+for (int j = 1; j <= 3; j++)
+{ 
+    Console.WriteLine($"FOR Variabile: {j}");
+}
+
+//Definizione
+int i = 1;
+while (i <= 3)
+{
+    Console.WriteLine($"WHILE Variabile: {i}");
+    if(i == 2){
+        break;
+    }
+    i++;
+}
+
+// Fai e poi verifica
+int k = 1;
+do
+{
+    Console.WriteLine($"DO WHILE Variabile: {k}");
+    k++;
+} while (k <= 3);
+
+
+Console.WriteLine($"Ciao");
+
+
+
