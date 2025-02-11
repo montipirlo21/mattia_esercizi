4b43d72764888f0b7d2b86b38445a1dcc35cd0c9
Davide Monticelli <davide.monticelli@fortech.it>
2025-02-11 17:10:45 +0100

    aggiunto esercizio 4

---
+++ Esercizio4/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Creare un programma che stampi un menu.
+// il menu compare una sola volta per ogni volta che lanci il programma
+// Il menu deve avere le seguenti opzioni:
+// 1. Chiedo di inserire un numero e lo deve stampare moltiplicato * 2
+// 2. Chiedo di inserire un numero e lo devo stampare moltiplicato per se stesso
+
+public partial class Program
+{
+    // All'interno di questa funzione dovrai stampare il menu e restituire il valore scelto dall'utente
+    public static int MostraMenu(){
+
+        // Stampo il menu
+        // Leggo la scelta.
+        return 0;
+    }    
+
+    public static void Main()
+    {
+        // Stampo il menu richiamando la funzione 
+
+        // In base alla scelta effettuo l'operazione riesta 
+        // 1:
+
+        // 2:
+        
+        // Stampo un "arrivederci"
+        
+    }
+}
+
