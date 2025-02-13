3a9625e42c2d9f52d9eb673bb7536314c8af4526
Davide Monticelli <davide.monticelli@hotmail.it>
2025-02-13 19:08:32 +0100

    aggiunto esercizio 5

--- Esercizio5/Program.cs
+++ Esercizio5/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public partial class Program
+{
+    // Dato un array, devi riuscire a invertire i dati dell'array senza usare un altro array
+    // ATTENZIONE a due cose: a come sostituisci i dati e quante iterazioni fai e a non sovrascriverti la roba mentre la sposti
+    public static void Main()
+    {   
+        int[] numeriOriginali = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }; 
+
+        for (int i = 0; i < numeriOriginali.Length; i++)
+        {
+            Console.Write(numeriOriginali[i] + " ");
+        }
+        Console.WriteLine();
+
+        // RISULTATO ATTESO
+        // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+        for (int i = 0; i < numeriOriginali.Length; i++)
+        {
+            Console.Write(numeriOriginali[i] + " ");
+        }
+        Console.WriteLine();
+    }   
+}
