f74c08e5c226d5f817c44a9b37829656693ffb8b
Davide Monticelli <davide.monticelli@hotmail.it>
2025-02-13 19:12:24 +0100

    aggiunto versione con numero dispari

--- Esercizio5/Program.cs
+++ Esercizio5/Program.cs
@@ -2,9 +2,13 @@
 {
     // Dato un array, devi riuscire a invertire i dati dell'array senza usare un altro array
     // ATTENZIONE a due cose: a come sostituisci i dati e quante iterazioni fai e a non sovrascriverti la roba mentre la sposti
+    // Puoi provare anche la versione con un numero dispari 
     public static void Main()
     {   
+        // Versione con numero pari
         int[] numeriOriginali = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }; 
+        // Versione con numero dispari di elementi
+        // int[] numeriOriginali = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }; 
 
         for (int i = 0; i < numeriOriginali.Length; i++)
         {
@@ -12,8 +16,10 @@
         }
         Console.WriteLine();
 
-        // RISULTATO ATTESO
+        // RISULTATO ATTESO 
         // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        // RISULTATO ATTESO Versione con numero dispari di elementi
+        // 1, 2, 3, 4, 5, 6, 7, 8, 9
 
         for (int i = 0; i < numeriOriginali.Length; i++)
         {
